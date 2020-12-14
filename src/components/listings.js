e9bdf9ad8947190da459aec805bfca79bad1bf5f
JustinBarsalou <justinbarsalou@gmail.com>
2020-12-14 17:02:23 -0600

    heroku again

--- src/components/listings.js
+++ src/components/listings.js
@@ -9,7 +9,6 @@ import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
 import { Link } from 'react-router-dom';
-import Button from '@material-ui/core/button';
 import AddListing from '../containers/addListing'
 import DeleteIcon from '@material-ui/icons/Delete';
 
